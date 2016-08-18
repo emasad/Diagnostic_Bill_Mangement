58e0b75ddf46b9a3dea9b545cc3dd50507d05239
emasad <e.md.asad@gmail.com>
2016-08-18 17:38:34 +0600

    Test BLL, DAL, MODEL.

---
+++ DiagnosticBillManagementApp/DiagnosticBillManagementApp/MODEL/TestClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiagnosticBillManagementApp.MODEL
+{
+    public class TestClass
+    {
+    }
+}
